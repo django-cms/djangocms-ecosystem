23d317cca42b220922f68186bdb87cdb2e7e520f
Fabian Braun <fsbraun@gmx.de>
2026-03-13 11:50:56 +0100

    fix: panel design, add deprecated plugin

--- djangocms_ecosystem/cms_plugins.py
+++ djangocms_ecosystem/cms_plugins.py
@@ -3,7 +3,7 @@ from cms.plugin_pool import plugin_pool
 
 from django.utils.translation import gettext_lazy as _
 
-from djangocms_ecosystem import get_chapter
+from djangocms_ecosystem import get_chapter, read_ecosystem
 
 
 @plugin_pool.register_plugin
@@ -29,4 +29,21 @@ class DjangoPackagesPlugin(CMSPluginBase):
         context.update({
             "content": get_chapter("Django packages").get("content", []),
         })
+        return context
+
+
+@plugin_pool.register_plugin
+class DeprecatedPackagesPlugin(CMSPluginBase):
+    render_template = "djangocms_ecosystem/deprecated.html"
+    name = _("Deprecated packages")
+    show_add_form = False
+
+    def render(self, context, instance, placeholder):
+        deprecated = [
+            package
+            for chapter in read_ecosystem()
+            for package in chapter.get("content", [])
+            if package.get("properties", {}).get("deprecated", False)
+        ]
+        context.update({"content": deprecated})
         return context
